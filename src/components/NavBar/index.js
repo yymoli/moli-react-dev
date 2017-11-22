31af62dfe9b453e0ee8e48d155ed1c1d5b1478c4
董政凯 <dongzhk@yonyou.com>
2017-11-22 14:32:07 +0800

    [update] 更新文件路径大小写问题

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-import Icon from '../icon/index'
+import Icon from '../Icon/index'
 import "./index.less"
 const defaultProps = {
   prefixCls: 'lebra-navbar',
